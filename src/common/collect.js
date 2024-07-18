39fadd501832a6a25fa96caa5c3db335d614d760
pekonchan <pekonchan@163.com>
2024-07-18 20:03:08 +0800

    fix: 词条变更再次打包构建需要更新词条配置文件

--- src/common/collect.js
+++ src/common/collect.js
@@ -276,10 +276,14 @@ export const updateResourceMap = () => {
     // Handle config
     if (!configNeedUpdate) {
         const newConfig = createConfigbyMap()
-        for (const key in newConfig) {
-            if (newConfig[key].value !== lastLocaleWordConfig[key]) {
-                configNeedUpdate = true
-                break
+        if (Object.keys(newConfig).length !== Object.keys(lastLocaleWordConfig).length) {
+            configNeedUpdate = true
+        } else {
+            for (const key in newConfig) {
+                if (newConfig[key].value !== lastLocaleWordConfig[key]) {
+                    configNeedUpdate = true
+                    break
+                }
             }
         }
     }
